33a109c44552ab010528f5cb2dd5eb45e461e7df
mhowlader <33489142+mhowlader@users.noreply.github.com>
2017-11-13 23:58:19 -0500

    Update Goblin.java

--- v3/Goblin.java
+++ v3/Goblin.java
@@ -1,9 +1,13 @@
 public class Goblin extends Monster{ 
-  super(){
-    attackr = .5;
-    life = 75;
+
+	public Goblin() {
+		
+		super();
+		attackr = .5;
+		life = 75;
     }
   public static String about(){
-    return "Goblin is a monster class that has high attack rating, low HP, and regular defense."
+    return "Goblin is a monster class that has high attack rating, low HP, and regular defense.";
   }
 }
+
