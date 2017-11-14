3705fa78cad583065916f40e7d31b9fc310de06a
mhowlader <33489142+mhowlader@users.noreply.github.com>
2017-11-13 23:57:51 -0500

    Update Berserker.java

--- v3/Berserker.java
+++ v3/Berserker.java
@@ -1,13 +1,16 @@
-public class Berserker extends Protagonist{ 
-  public Berserker(){
+public class Mage extends Protagonist{ 
+  public Mage(String name){
+
     super(name);
-    attackr = .7;
-    life = 75;
+    life = 175;
+    defense = 25;
   }
+  public void normalize(){
+    defense = 25;
+  }
+
   public static String about(){
-    return "Berserker is a hero class that has high attack rating, low HP, and regular defense."
-  }
-  public void normalize() {
-    attackr = .7;
+    return "Mage is a hero class that has high HP, low defense, and regular attack rating.";
   }
 }
+
