c58a56e40cde5044af7d4491c4586ed4fb4433a5
HasifAhmed <33489092+HasifAhmed@users.noreply.github.com>
2017-11-13 22:03:23 -0500

    Update MageM.java

--- v3/MageM.java
+++ v3/MageM.java
@@ -4,4 +4,7 @@ public class MageM extends Monster{
     life = 150;
     defense = 25;
   }
+  public static String about(){
+    return "Mage is a monster class that has high HP, low defense, and regular attack rating."
+  }
 }
