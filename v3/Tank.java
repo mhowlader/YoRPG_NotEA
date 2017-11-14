b2f817453966f62f72e885cf576b747e765558b4
HasifAhmed <33489092+HasifAhmed@users.noreply.github.com>
2017-11-13 22:09:10 -0500

    Update Tank.java

--- v3/Tank.java
+++ v3/Tank.java
@@ -1,7 +1,6 @@
 public class Tank extends Protagonist {
-  private String name = "BigMan";
   public Tank() {
-    super("BigMan");
+    super(name);
     attackr = 0.2;
     defense = 70; }
   public void normalize() {
