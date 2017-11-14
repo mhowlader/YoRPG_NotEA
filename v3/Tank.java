2f17c51938b29a90d4b5da4316414884438090de
kyletau67 <33489148+kyletau67@users.noreply.github.com>
2017-11-13 19:58:09 -0500

    Tank-Defense Protagonist

--- v3/Tank.java
+++ v3/Tank.java
@@ -1,7 +1,7 @@
 public class Tank extends Protagonist {
   private String name = "BigMan";
   public Tank() {
-    super();
+    super("BigMan");
     attack = 0.2;
     defense = 70; }
   public void normalize() {
