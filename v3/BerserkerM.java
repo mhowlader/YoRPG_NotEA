c75f6a9b4265a5314ca7574385c3ac60aa3ce773
HasifAhmed <33489092+HasifAhmed@users.noreply.github.com>
2017-11-13 22:03:04 -0500

    Update BerserkerM.java

--- v3/BerserkerM.java
+++ v3/BerserkerM.java
@@ -4,6 +4,6 @@ public class BerserkerM extends Monster{
     life = 75;
     }
   public static String about(){
-    return "Goblin is a hero class that has high attack rating, low HP, and regular defense."
+    return "Goblin is a monster class that has high attack rating, low HP, and regular defense."
   }
 }
