86e74e0a7fdfa420114f5533ae143b7b0e7ac245
HasifAhmed <33489092+HasifAhmed@users.noreply.github.com>
2017-11-13 22:02:52 -0500

    Update Mage.java

--- v3/Mage.java
+++ v3/Mage.java
@@ -8,4 +8,7 @@ public class Mage extends Protagonist{
   public void normalize(){
     defense = 25;
   }
+  public static String about(){
+    return "Mage is a hero class that has high HP, low defense, and regular attack rating."
+  }
 }
