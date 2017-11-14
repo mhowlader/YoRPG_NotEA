64c98efd6f6b332f5e036e4e321831bf1befac18
HasifAhmed <33489092+HasifAhmed@users.noreply.github.com>
2017-11-13 22:04:37 -0500

    Update Tank.java

--- v3/Tank.java
+++ v3/Tank.java
@@ -7,4 +7,7 @@ public class Tank extends Protagonist {
   public void normalize() {
     attackr = 0.2;
     defense = 70; }
+  public static String about(){
+    return "Tank is a hero class that has high defense, low attack rating, and regular HP."
+  }
     }
