647e9a4ca95fe609960b5e6da2b07e7775935a67
HasifAhmed <33489092+HasifAhmed@users.noreply.github.com>
2017-11-13 21:34:47 -0500

    Update Berserker.java

--- v3/Berserker.java
+++ v3/Berserker.java
@@ -2,6 +2,11 @@ public class Berserker extends Protagonist{
   public String name = "AngryMan";
   public Berserker(){
     super(name);
-    attackr = 
-
+    attackr = .7;
+    defense = 25;
+  }
+  public void normalize() {
+    attackr = .7;
+    defense = 25;
+  }
 }
