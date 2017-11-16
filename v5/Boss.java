a6f129852a21dff86eec82838d9042640155844b
mhowlader <33489142+mhowlader@users.noreply.github.com>
2017-11-16 04:17:00 -0500

    Update Boss.java

--- v5/Boss.java
+++ v5/Boss.java
@@ -1,15 +1,11 @@
 public class Boss extends Monster{
     public Boss(){
 	super();
-	attackr = .5;
+	attackr = .8;
 	defense = 50;
 	life = 150;
     }
-    public int attackspecial(Protagonist x){
-	int damage = ((int)((strength * attackr) * 1.2)) - x.getDefense();
-	x.lowerHP(damage);
-	return damage;
-    }
+    
    public static String about(){
     return "Boss is a monster class that has all the high traits of each monster class. He also has two attacks, his special and the normal attack. His special takes two turns to charge up.";
   }
