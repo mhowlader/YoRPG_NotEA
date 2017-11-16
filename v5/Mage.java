60cdc0c16af9e48248c5b45977bf6e94d0bf3f8e
mhowlader <33489142+mhowlader@users.noreply.github.com>
2017-11-16 04:19:33 -0500

    Update Mage.java

--- v5/Mage.java
+++ v5/Mage.java
@@ -14,30 +14,38 @@ public class Mage extends Protagonist{
 	  attackr+=0.20;
   }
   
-  public void classSpecial() {
-	  if (Math.random() > 0.7) {
-	  	life += 65;}
-  	  else {
-	  	normalize();
-	  }
-  }
+
   public String about(){
     return "Mage is a hero class that has high HP, low defense, and regular attack rating.";
   }
   
-  /*public String firstAttName() {
-	return "Meditation";
+  public String firstAttName() {
+	return "Fireball Spell";
   }
   
   public String secAttName() {
-	return "Sec";
+	return "Enhanced Fireball Spell";
   }
   
   public String thirAttName() {
-	return "thir";
+	return "Regeneration";
 	
-  }*/
+  }
   
+  public void secAtt() {
+	  if (Math.random() <0.5) { //50% chance of being beneficial
+		  defense += 30;
+		  System.out.println("Defense has increased!");
+	  }
+	  else {
+		  defense-=15;
+		  System.out.println("Defense has decreased!");
+	  }
+  }
+  
+  public void thirAtt() {
+	  life+=60;
+  }
   
 }
 
