085b4407b224b16190f9b73de3aefba0b7185780
mhowlader <33489142+mhowlader@users.noreply.github.com>
2017-11-16 04:13:43 -0500

    Update Tank.java

--- v5/Tank.java
+++ v5/Tank.java
@@ -15,29 +15,36 @@ public class Tank extends Protagonist {
 	  attackr+=0.3;
   }
   
-  public void classSpecial() {
-	  if (Math.random() > 0.7) {
-	  	defense += 50;
-	  	attackr -= 0.1;}
-  	  else {
-	  	normalize();
-	  }
-  }
-	  
+
   public String about() {
     return "Tank is a hero class that has high defense, low attack rating, and regular HP.";
   }
   
-  /*public String firstAttName() {
-	return "Meditation";
+  public String firstAttName() {
+	return "Blast";
   }
   
   public String secAttName() {
-	return "Sec";
+	return "Enhanced Blast";
   }
   
   public String thirAttName() {
-	return "thir";
+	return "Full Overdrive";
 	
-  }*/
+  }
+  
+  public void secAtt() {
+	  if (Math.random() <0.5) {
+		  defense +=20;
+	  }
+	  else {
+		  defense -= 10;
+	  }
+  }
+  
+  public void thirAtt() {
+	  defense+=40;
+	  attackr+=0.2;
+  }
+  
 }
